1b65fd1ffd3a42d53b94be953c34f4c16d95697d
yerszh <yers.zh@gmail.com>
2022-09-07 12:00:40 +0500

    comment deleted

--- js/script.js
+++ js/script.js
@@ -92,8 +92,6 @@ window.addEventListener('DOMContentLoaded', function() {
 
     setClock('.timer', deadline);
 
-    // Modal
-
      const modalTrigger = document.querySelectorAll('[data-modal]'),
         modal = document.querySelector('.modal');
 
@@ -194,33 +192,6 @@ window.addEventListener('DOMContentLoaded', function() {
             });
         });
     
-    // getResource('http://localhost:3000/menu')
-    //     .then(data => createCard(data));
-
-    // function createCard(data) {
-    //     data.forEach(({img, altimg, title, descr, price}) => {
-    //         const element = document.createElement('div');
-
-    //         element.classList.add('menu__item');
-
-    //         element.innerHTML = `
-    //             <img src=${img} alt=${altimg}>
-    //             <h3 class="menu__item-subtitle">${title}</h3>
-    //             <div class="menu__item-descr">${descr}</div>
-    //             <div class="menu__item-divider"></div>
-    //             <div class="menu__item-price">
-    //                 <div class="menu__item-cost">Цена:</div>
-    //                 <div class="menu__item-total"><span>${price}</span> KZT/день</div>
-    //             </div>
-    //         `;
-
-    //         document.querySelector('.menu .container').append(element);
-    //     })
-    // }
-
-
-
-    //Forms
 
     const forms = document.querySelectorAll('form');
     
